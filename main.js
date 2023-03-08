2946c4238f8f2acb7e8206a68d8d49b2d31bbf02
Wayne Yen <wayway1974@gmail.com>
2023-03-08 15:01:24 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const electron = require('electron')
+const { app, BrowserWindow, screen } = electron
+const { ipcMain } = electron
+const path = require('path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  })
+
+  win.webContents.openDevTools()
+  win.loadFile('index.html')
+
+  const displays = screen.getAllDisplays()
+  ipcMain.on('getDisplays', (event) => {
+    event.reply('displays', displays);
+  });
+
+  ipcMain.on('playFullScreen', (event, display, args) => {
+    const view = new electron.BrowserWindow({
+      x: display.bounds.x,
+      y: display.bounds.y,
+      width: display.size.width,
+      height: display.size.height,
+    })
+    view.setFullScreen(true)
+    view.loadURL(args)
+  })
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
---
+++ renderer.js
@@ -0,0 +1,24 @@
+const { BrowserView, ipcRenderer } = require('electron')
+
+let displays
+
+ipcRenderer.send('getDisplays');
+ipcRenderer.on('displays', (event, ds) => {
+  displays = ds
+
+  if (displays[0]) {
+    document.getElementById('play-first').disabled = false
+  }
+
+  if (displays[1]) {
+    document.getElementById('play-second').disabled = false
+  }
+})
+
+document.getElementById('play-first').addEventListener('click', () => {
+  playFullScreen(displays[0])
+})
+
+const playFullScreen = (display) => {
+  ipcRenderer.send('playFullScreen', display, document.getElementById('yt').src)
+}
